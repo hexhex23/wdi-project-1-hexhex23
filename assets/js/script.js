f4f579b3ee81ae4dc9e812a074b353b972051ac6
Maria <Maria>
2017-03-29 17:25:11 +0800

    Changed Button Sytles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+  // swal({
+  //   title: 'Error!',
+  //   text: 'Do you want to continue',
+  //   type: 'input',
+  //   confirmButtonText: 'Cool'
+  // })
+  //
+
+
     //array of 52 playing cards
     arrayCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
 
@@ -297,8 +306,20 @@ $(document).ready(function() {
     //function for determining if 4 kings have been played
     function winningConditions() {
         if (arrayPlayed.includes(13) === true && arrayPlayed.includes(26) === true && arrayPlayed.includes(39) === true && arrayPlayed.includes(52) === true) {
-            alert("GAME OVER Last King Drawn: Down It!!")
-            window.location.reload(true)
+          swal({
+            title: 'GAME OVER',
+            text: '4th King Drawn: Drink The Dirty Pint',
+            timer: 10000
+            }).then(
+  function () {},
+  // handling the promise rejection
+  function (dismiss) {
+    if (dismiss === 'timer') {
+      console.log('I was closed by the timer')
+    }
+  }
+)
+        window.location.reload(true)
         }
     }
 
